c82ed799f30403a148db1c5f97a74e10c2c9a46e
Alexey Samoylov <alexey.samoylov@gmail.com>
2025-12-29 11:22:48 +0600

    added output file support for better go tool experience

--- cmd/tgbotspec/main.go
+++ cmd/tgbotspec/main.go
@@ -15,18 +15,39 @@ var (
 )
 
 func newRootCmd() *cobra.Command {
+	var outputPath string
+
 	cmd := &cobra.Command{
 		Use:          "tgbotspec",
 		Short:        "Generate an OpenAPI spec for the Telegram Bot API",
 		SilenceUsage: true,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			if err := runScraper(cmd.OutOrStdout()); err != nil {
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			output := cmd.OutOrStdout()
+
+			if outputPath != "" {
+				file, err := os.Create(outputPath)
+				if err != nil {
+					return fmt.Errorf("create output file: %w", err)
+				}
+
+				defer func() {
+					if closeErr := file.Close(); closeErr != nil && err == nil {
+						err = fmt.Errorf("close output file: %w", closeErr)
+					}
+				}()
+
+				output = file
+			}
+
+			if err := runScraper(output); err != nil {
 				return fmt.Errorf("run scraper: %w", err)
 			}
 
 			return nil
 		},
 	}
+
+	cmd.Flags().StringVarP(&outputPath, "output", "o", "", "Write output to file instead of stdout")
 
 	return cmd
 }
